0db99c81aad1e6b60e9689789770d8559aa6f82a
Damon Kalaboukis <damonkala@yahoo.com>
2016-01-14 17:55:10 -0800

    Comments now show

--- public/javascripts/controllers/posts/postShow.js
+++ public/javascripts/controllers/posts/postShow.js
@@ -6,5 +6,7 @@ app.controller('postShowCtrl', function($scope, $state, $http, ENV, PostService)
   PostService.show($state.params.postId)
   .then(function(res) {
     $scope.post = res.data;
+    console.log(res.data.comments);
+    $scope.comments = res.data.comments;
   });
 });
